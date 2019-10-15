f6329594afe258cdeb16c9ac200a74e273a20876
Jisha-George <jishathakidiyel@yahoo.com>
2019-10-15 18:05:54 +0100

    Automatic Commit

--- catkin_ws/src/ass1_1/scripts/test.py
+++ catkin_ws/src/ass1_1/scripts/test.py
@@ -1,98 +1,71 @@
 #!/usr/bin/env python
 
-#>>> green = np.uint8([[[0,255,0 ]]])
-#>>> hsv_green = cv2.cvtColor(green,cv2.COLOR_BGR2HSV)
-#>>> print hsv_green
+'''
+Video histogram sample to show live histogram of video
+Keys:
+    ESC    - exit
+'''
 
-# USE ABOVE TO CALCULATE COLOUR VALUES IN HSV
-# lower green = 50, 100, 100
-# upper green = 70, 255, 255
+# Python 2/3 compatibility
+from __future__ import print_function
 
-#lower blue = 110, 100, 100
-#upper blue = 130, 255, 255
+import numpy as np
+import cv2 as cv
 
-#lower red = 0, 100, 100
-#upper red = 10, 255, 255
+# built-in modules
+import sys
 
-#lower yellow = 20, 100, 100
-#upper yellow = 40, 255, 255
+# local modules
+#import video
 
-import numpy
+class App():
 
-import cv2
-import cv_bridge
-import rospy
+    def set_scale(self, val):
+        self.hist_scale = val
 
-from sensor_msgs.msg import Image
-from geometry_msgs.msg import Twist
-from cv_bridge import CvBridge, CvBridgeError
+    def run(self):
+        hsv_map = np.zeros((180, 256, 3), np.uint8)
+        h, s = np.indices(hsv_map.shape[:2])
+        hsv_map[:,:,0] = h
+        hsv_map[:,:,1] = s
+        hsv_map[:,:,2] = 255
+        hsv_map = cv.cvtColor(hsv_map, cv.COLOR_HSV2BGR)
+        cv.imshow('hsv_map', hsv_map)
 
-kernel = numpy.ones((10, 10), numpy.uint8)
+        cv.namedWindow('hist', 0)
+        self.hist_scale = 10
 
-class Follower:
+        cv.createTrackbar('scale', 'hist', self.hist_scale, 32, self.set_scale)
 
-    def __init__(self):
+        try:
+            fn = sys.argv[1]
+        except:
+            fn = 0
+        #cam = video.create_capture(fn, fallback='synth:bg=baboon.jpg:class=chess:noise=0.05')
 
-        self.bridge = cv_bridge.CvBridge()
-        self.image_sub = rospy.Subscriber('thorvald_002/kinect2_camera/hd/image_color_rect', Image,
-                                          self.image_callback)
-        self.cmd_vel_pub = rospy.Publisher('thorvald_002/teleop_joy/cmd_vel', Twist,
-                                           queue_size=1)
-        #self.maskPub = rospy.Publisher('/13488071/images/mask', Image, queue_size = 1)
-        self.twist = Twist()
-	
-    def image_callback(self, msg):
-    	print("Hi")
-        cv2.namedWindow("window", 1)
-        image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        while True:
+           # flag, frame = cam.read()
+            cv.imshow('camera', frame)
 
-        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        
-        gre = cv2.inRange(hsv, numpy.array([30, 0, 0]), numpy.array([130, 255, 255]))
+            small = cv.pyrDown(frame)
 
-        mask = gre
-        h, w, d = image.shape
-        #search_top = 0
-        #search_bot = h
-        #mask[0:search_top, 0:w] = 0
-        #mask[search_bot:h, 0:w] = 0
+            hsv = cv.cvtColor(small, cv.COLOR_BGR2HSV)
+            dark = hsv[...,2] < 32
+            hsv[dark] = 0
+            h = cv.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
 
-        mask = cv2.erode(mask, kernel)
-        #M = cv2.moments(mask)
-	#found = 3
+            h = np.clip(h*0.005*self.hist_scale, 0, 1)
+            vis = hsv_map*h[:,:,np.newaxis] / 255.0
+            cv.imshow('hist', vis)
 
-	# if M['m00'] == 0:
-	# 	self.twist.angular.z = 0.5
-	# 	self.twist.linear.x = 0
-	# 	self.cmd_vel_pub.publish(self.twist)
-	# 	print('twisting')
-	# else:
-	#     cx = int(M['m10']/M['m00'])
-	#     cy = int(M['m01']/M['m00'])
-	#     cv2.circle(image, (cx, cy), 20, (0, 0, 255), -1)
-	#     err = cx - w/2
+            ch = cv.waitKey(1)
+            if ch == 27:
+                break
 
-	#     if mask[h-1, w/2] != 0:
-	# 	self.twist.linear.x = 0
-	# 	#stop 1m before cylinder if the bottom middle of the mask is white
-	#     else:
-	# 	self.twist.linear.x = 0.5
-    
-	#     self.twist.angular.z = -float(err) / 100
-	#     print self.twist.angular.z
-	#     #print(found)
+        print('Done')
 
- #    	    self.cmd_vel_pub.publish(self.twist)
 
-        masked_image = cv2.bitwise_and(image, image, mask=mask)
-
-        cv2.imshow("image", image)
-        cv2.imshow("mask", masked_image)
-        cv2.waitKey(3)
-
-cv2.startWindowThread()
-rospy.init_node('follower')
-follower = Follower()
-rospy.spin()
-
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    print(__doc__)
+    App().run()
+    cv.destroyAllWindows()
