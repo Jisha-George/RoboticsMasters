8a3c07d6019d461cc51ef5e26d284c637c619510
Jisha-George <jishathakidiyel@yahoo.com>
2019-10-29 18:04:48 +0000

    Automatic Commit

--- catkin_ws/build/ass1_1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/computing/RoboticsM/catkin_ws/src/ass1_1
-# Build directory: /home/computing/RoboticsM/catkin_ws/build/ass1_1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
