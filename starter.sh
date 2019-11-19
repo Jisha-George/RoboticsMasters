e68dd2a039dcfc299fc8a6ed57e1655de4169948
Jisha-George <jishathakidiyel@yahoo.com>
2019-11-19 18:06:08 +0000

    Automatic Commit

--- starter.sh
+++ starter.sh
@@ -10,21 +10,21 @@ wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 
-sudo apt-get update
-
-sudo apt-get install atom
-
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-
-sudo apt-get install apt-transport-https
-
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt
 
 sudo apt-get update
 
+sudo apt-get install atom
+
+sudo apt-get install apt-transport-https
+
 sudo apt-get install sublime-text
 
-sudo apt-get upgrade
+sudo apt-get install ros-kinetic-uol-cmp9767m-base
+sudo apt-get install ros-kinetic-uol-cmp9767m-tutorial
+
+sudo apt-get update && sudo apt-get upgrade
 #git clone https://github.com/Jisha-George/RoboticsM
 
 #git clone https://github.com/Jisha-George/HackRF
